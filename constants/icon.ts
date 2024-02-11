7cd866a96f70edffb322a03fb6a3f91d4fbf8bee
heisarkiphawa <heisarki.p@codematix.com>
2024-02-11 14:51:14 +0530

    fix: build issue

--- constants/icon.ts
+++ constants/icon.ts
@@ -1,6 +1,6 @@
-import profileIcon from '../../assets/svg/proflie.svg'
-import teamIcon from '../../assets/svg/team.svg'
-import homeIcon from '../../assets/svg/home.svg'
+import profileIcon from '../assets/svg/proflie.svg'
+import teamIcon from '../assets/svg/team.svg'
+import homeIcon from '../assets/svg/home.svg'
 
 export {
     profileIcon,
