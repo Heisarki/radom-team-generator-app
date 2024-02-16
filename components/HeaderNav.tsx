272b51fe8aa11d45c05cb822b3b26e193cc7bf8b
heisarkiphawa <heisarki.p@codematix.com>
2024-02-16 20:46:51 +0530

    fix: coflict after pulling from main branch

--- components/HeaderNav.tsx
+++ components/HeaderNav.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { profileIcon } from '@/constants/icon'
 import Image from 'next/image'
-import ThemToggle from './ui/ThemToggle'
 import { ModeToggle } from './ui/mode-toggle'
 
 export default function HeaderNav() {
