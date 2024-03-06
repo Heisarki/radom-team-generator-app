707cd2f308ed5a3379dc370e90d05877c1e82683
heisarkiphawa <heisarki.p@codematix.com>
2024-03-06 21:44:59 +0530

    feat: adding Typing animation for placeholder

--- components/createTeam/SearchInputWithSetting.tsx
+++ components/createTeam/SearchInputWithSetting.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { PlayerListContextType, usePlayerListContext } from '@/context/PlayerListContext'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from '../ui/input'
 import { GoGear } from "react-icons/go";
 import { CreateTeamSettingsContextType, useCreateTeamSettingsContext } from '@/context/CreateTeamSettingContext';
@@ -13,6 +13,26 @@ export default function SearchInputWithSetting() {
     const {
         setOpenCreatedTeamSettingsDialog
     }: CreateTeamSettingsContextType = useCreateTeamSettingsContext();
+    /**
+     * Typing animation for placeholder
+     */
+    const [currentText, setCurrentText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const text = "Search player here  "
+    useEffect(() => {
+        let delay = 70
+        if (currentIndex === text.length - 1)
+            delay = 2000
+        const timeout = setTimeout(() => {
+            setCurrentText(prevText => prevText + text[currentIndex]);
+            setCurrentIndex(prevIndex => prevIndex + 1);
+        }, delay);
+        if (currentIndex === text.length) {
+            setCurrentIndex(0)
+            setCurrentText("")
+        }
+        return () => clearTimeout(timeout);
+    }, [currentIndex]);
     return (
         <div className="sticky top-16 pb-3 bg-background pt-2 flex flex-row gap-2 items-center w-full">
             <Input
@@ -20,13 +40,11 @@ export default function SearchInputWithSetting() {
                 className='px-5 py-6'
                 value={searchValue}
                 onChange={onSeachInputChange}
-                placeholder='Search player'
+                placeholder={currentText}
             />
             <div className="border p-3 rounded-lg cursor-pointer" onClick={() => setOpenCreatedTeamSettingsDialog(true)}>
                 <GoGear size="24px" />
             </div>
         </div>
-
-
     )
 }
